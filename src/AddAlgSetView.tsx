2a1e524ed614baaa1eea595f3fb6598df4b01f32
pnaimoli <pnaimoli@users.noreply.github.com>
2024-06-18 22:02:25 -0500

    Support some algorithm presets.
    Just PLL for now.

--- src/AddAlgSetView.tsx
+++ src/AddAlgSetView.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
-import { Textarea, Button, TextInput, Group, Box, Text } from "@mantine/core";
+import { Textarea, Button, TextInput, Group, Box, Text, List } from "@mantine/core";
 import Papa from 'papaparse';
 
 import { SolvedState, ValidMove, Alg, AlgSet, SOLVED_STATES } from './interfaces';
+import { ALG_PRESETS } from './algPresets';
 
 interface AddAlgSetViewProps {
   algSets: AlgSet[];
@@ -30,7 +31,7 @@ const AddAlgSetView: React.FC<AddAlgSetViewProps> = ({ algSets, setAlgSets }) =>
       parsedData = Papa.parse(currentInput.trim(), {
         delimiter: ",",
         skipEmptyLines: true,
-        transform: (value) => value.trim()
+        transform: (value) => value.trim().replace(/[()]/g, '')
       }).data as string[][];
     } catch (error) {
       console.error("Error parsing CSV:", error);
@@ -47,14 +48,14 @@ const AddAlgSetView: React.FC<AddAlgSetViewProps> = ({ algSets, setAlgSets }) =>
         const [name, alg, solved = 'full'] = line;
         const algMoves = alg.split(/\s+/).map(move => {
           if (!Object.values(ValidMove).includes(move as ValidMove)) {
-            throw new Error(`Invalid move found in algorithm: ${move}`);
+            throw new Error(`Invalid move found in algorithm: ${move}, line: ${line}`);
           }
           return move as ValidMove;
         });
 
         const solvedLower = solved.trim().toLowerCase();
         if (!Object.values(SolvedState).includes(solvedLower as SolvedState)) {
-          throw new Error(`Invalid solved state: ${solved}`);
+          throw new Error(`Invalid solved state: ${solved}, line: ${line}`);
         }
 
         algs.push({ name: name.trim(), alg: algMoves, solved: solvedLower as SolvedState });
@@ -72,6 +73,14 @@ const AddAlgSetView: React.FC<AddAlgSetViewProps> = ({ algSets, setAlgSets }) =>
     setNameExists(algSets.some(set => set.name === newFolderName));
   };
 
+  const insertPreset = (preset: string) => {
+    if (textareaRef.current) {
+      textareaRef.current.value = preset.trim();
+    }
+  };
+
+  const uniqueValidMoves = Array.from(new Set(Object.values(ValidMove).map(move => move.charAt(0))));
+
   return (
     <Box>
       <Button
@@ -84,36 +93,30 @@ const AddAlgSetView: React.FC<AddAlgSetViewProps> = ({ algSets, setAlgSets }) =>
       {showInstructions && (
         <Box my="sm" style={{ padding: '15px', border: '1px solid #ddd', borderRadius: '5px' }}>
           <Text weight={700} mb="xs" style={{ fontSize: '20px' }}>Algorithm Input Format</Text>
-          <Box style={{ paddingLeft: '20px', color: '#555', lineHeight: '1.6' }}>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>1. List each algorithm on a separate line.</strong></Text>
-            </Box>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>2. Separate each move by at least one space.</strong></Text>
-            </Box>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>3. Start with the name of the algorithm, followed by a colon, and then the algorithm in quotations, like this:</strong></Text>
+          <List spacing="sm" type="ordered" style={{ color: '#555', lineHeight: '1.6' }}>
+            <List.Item>
+              <Text><strong>List each algorithm on a separate line.</strong></Text>
+            </List.Item>
+            <List.Item>
+              <Text><strong>Separate each move by at least one space.</strong></Text>
+            </List.Item>
+            <List.Item>
+              <Text><strong>Start with a unique name for the algorithm, followed by a comma, and then the algorithm, like this:</strong></Text>
               <Box ml="md" my="xs">
-                <Text style={{ fontFamily: 'monospace', color: '#e67e22' }}>T: "R U R' U' R' F R2 U' R' U' R U R' F'"</Text>
-                <Text style={{ fontFamily: 'monospace', color: '#e67e22' }}>J: "R U R' F' R U R' U' R' F R2 U' R' U' R U R' F'"</Text>
+                <Text style={{ fontFamily: 'monospace', color: '#e67e22' }}>H, &nbsp;M2 U M2 U2 M2 U M2</Text>
+                <Text style={{ fontFamily: 'monospace', color: '#e67e22' }}>Ja, L' U' L F L' U' L U L F' L2 U L</Text>
               </Box>
-            </Box>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>4. Assign a unique name to each algorithm</strong> (times are saved for each name, so if you want unique times for every algorithm use unique names).</Text>
-            </Box>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>5. Only use the following moves</strong> (', 2, or 3 can be added to the end, but nothing 4 or above):</Text>
+            </List.Item>
+            <List.Item>
+              <Text><strong>Only use the following moves</strong> (', 2, or 3 can be added to the end, but nothing 4 or above):</Text>
               <Box ml="md" my="xs">
-                <Text style={{ fontFamily: 'monospace', color: '#3498db' }}>R L U D F B M S E r l u d f b</Text>
+                <Text style={{ fontFamily: 'monospace', color: '#3498db' }}>{uniqueValidMoves.join(' ')}</Text>
               </Box>
-            </Box>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>6. Use a maximum of 1000 algorithms.</strong></Text>
-            </Box>
-            <Box style={{ marginBottom: '10px' }}>
-              <Text><strong>7. Most algorithms ranging from F2L, last layer, blindfolded, FMC and more are usable!</strong></Text>
-            </Box>
-          </Box>
+            </List.Item>
+            <List.Item>
+              <Text><strong>Most algorithms ranging from F2L, last layer, blindfolded, FMC and more are usable!</strong></Text>
+            </List.Item>
+          </List>
         </Box>
       )}
       <TextInput
@@ -130,11 +133,27 @@ const AddAlgSetView: React.FC<AddAlgSetViewProps> = ({ algSets, setAlgSets }) =>
       <Textarea
         ref={textareaRef}
         label="Algorithm List"
-        description="see instructions for formatting"
+        description={
+          <>
+            Presets:
+            {Object.keys(ALG_PRESETS).map(preset => (
+              <Button
+                key={preset}
+                size="xs"
+                variant="outline"
+                style={{ marginLeft: '10px' }}
+                onClick={() => insertPreset(ALG_PRESETS[preset])}
+              >
+                {preset}
+              </Button>
+            ))}
+            </>
+        }
         placeholder={`T: "R U R' U' R' F R2 U' R' U' R U R' F'"`}
         minRows={15}
         maxRows={15}
         autosize
+        styles={{ input: { fontFamily: 'monospace' } }}
         style={{ marginBottom: '10px' }}
       />
       <Group position="right">
---
+++ src/algPresets.ts
@@ -0,0 +1,25 @@
+export const ALG_PRESETS = {
+	"PLL": `
+Aa, x (R' U R') D2 (R U' R') D2 R2 x'
+Ab, x R2 D2 (R U R') D2 (R U' R) x'
+E,  x' R U' R' D R U R' D' R U R' D R U' R' D' x
+F,  R' F R f' R' F R2 U R' U' R' F' R2 U R' S
+Ga, R2 U R' U R' U' R U' R2 D U' R' U R D'
+Gb, R' U' R U D' R2 U R' U R U' R U' R2 D
+Gc, R2 F2 R U2 R U2 R' F R U R' U' R' F R2
+Gd, R U R' U' D R2 U' R U' R' U R' U R2 D'
+H,  M2 U M2 U2 M2 U M2
+Ja, L' U' L F L' U' L U L F' L2 U L
+Jb, R U R' F' R U R' U' R' F R2 U' R'
+Na, z U R' D R2 U' R D' U R' D R2 U' R D' z'
+Nb, R' U R' F R F' R U' R' F' U F R U R' U' R
+Ra, R U' R2 D' R U R' D R U' R U' R' U R U R'
+Rb, R' U2 R U2 R' F R U R' U' R' F' R2
+T,  R U R' U' R' F R2 U' R' U' R U R' F'
+Ua, R U' R U R U R U' R' U' R2
+Ub, R2' U R U R' U' R3 U' R' U R'
+V,  R' U R U' R' f' U' R U2 R' U' R U' R' f R
+Y,  F R (U' R' U') (R U R' F') (R U R' U') (R' F R F')
+Z,  M2 U M2 U M' U2 M2 U2 M'
+`
+};
