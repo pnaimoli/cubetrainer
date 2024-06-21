53e1775fc433bd3de23b44919fb6a4f0d95118d2
pnaimoli <pnaimoli@users.noreply.github.com>
2024-06-21 17:56:10 -0500

    Add some more mirror unit tests.

--- src/__tests__/CTAlg.test.ts
+++ src/__tests__/CTAlg.test.ts
@@ -8,9 +8,21 @@ describe('CTAlg', () => {
     expect(mirrored).to.equal("L' U' L U");
   });
 
+  it('should mirror moves correctly', () => {
+    const alg = new CTAlg("x R' U R' D2 R U' R' D2 R2 x'");
+    const mirrored = alg.mirror().toString();
+    expect(mirrored).to.equal("x L U' L D2' L' U L D2' L2' x'");
+  });
+
   it('should mirror over S moves correctly', () => {
     const alg = new CTAlg("F R U R' U' F'");
     const mirrored = alg.mirrorOverS().toString();
     expect(mirrored).to.equal("B' R' U' R U B");
+  });
+
+  it('should mirror over S moves correctly', () => {
+    const alg = new CTAlg("x R' U R' D2 R U' R' D2 R2' x'");
+    const mirrored = alg.mirrorOverS().toString();
+    expect(mirrored).to.equal("x' R U' R D2' R' U R D2' R2 x");
   });
 });
